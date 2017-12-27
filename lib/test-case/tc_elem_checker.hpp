365d7913858f371905ff4ef13be02ba3baa06608
Bo Chen <chenbo@pdx.edu>
2017-12-27 10:52:53 -0800

    Add list of pointer-return functions to tc_elem_checker.hpp

--- lib/test-case/tc_elem_checker.hpp
+++ lib/test-case/tc_elem_checker.hpp
@@ -12,9 +12,49 @@
 
 using namespace std;
 
+// Function return as pointer values
+static vector<string> init_ptr_ret_funcs()
+{
+    static vector<string> list;
+
+    list.push_back("__alloc_ei_netdev");
+    list.push_back("alloc_etherdev_mqs");
+    list.push_back("alloc_pages_current");
+    list.push_back("__alloc_skb");
+    list.push_back("dma_pool_alloc");
+    list.push_back("__kmalloc");
+    list.push_back("kmem_cache_alloc_trace");
+    list.push_back("__napi_alloc_skb");
+    list.push_back("netdev_alloc_frag");
+    list.push_back("__netdev_alloc_skb");
+    list.push_back("scsi_host_alloc");
+    list.push_back("vzalloc");
+
+    list.push_back("pci_get_device");
+    list.push_back("pci_get_domain_bus_and_slot");
+    list.push_back("pci_iomap");
+    list.push_back("pci_ioremap_bar");
+    list.push_back("snd_pci_quirk_lookup");
+
+    return list;
+};
+
+const static vector<string> ptr_ret_funcs = init_ptr_ret_funcs();
+
+static inline
+bool match_ptr_ret_funcs(const string name)
+{
+    for(vector<string>::const_iterator it = ptr_ret_funcs.begin();
+            it != ptr_ret_funcs.end(); ++it) {
+        if(name.find(*it) != string::npos) return true;
+    }
+
+    return false;
+}
+
 // ------------------------------------------------
 static inline
-bool __CTCEM_alloc(const vector<uint8_t> &data)
+bool __CTCEM_ret_ptr(const vector<uint8_t> &data)
 {
     if(data == vector<uint8_t>(data.size(), 0))
     {
@@ -31,9 +71,9 @@ bool check_tc_elem_meaningfulness(const vector<uint8_t> &data,
 {
     bool ret = true;
 
-    if(name.find("alloc") != string::npos)
+    if(match_ptr_ret_funcs(name))
     {
-        ret = __CTCEM_alloc(data);
+        ret = __CTCEM_ret_ptr(data);
     }
 
     return ret;
