93685d0503e3b42d6052870748164a16b01764fe
Bo Chen <chenbo@pdx.edu>
2018-05-07 12:05:15 -0700

    Complete list of tc-elem for meaningfulness check

--- lib/test-case/tc_elem_checker.hpp
+++ lib/test-case/tc_elem_checker.hpp
@@ -9,49 +9,64 @@
 #define LIB_TEST_CASE_TC_ELEM_CHECKER_HPP_
 
 #include <vector>
+#include <assert.h>
 
 using namespace std;
 
 // Function return as pointer values
-static vector<string> init_ptr_ret_funcs()
+static set<string> init_ptr_ret_funcs()
 {
-    static vector<string> list;
+    set<string> list;
 
-    list.push_back("__alloc_ei_netdev");
-    list.push_back("alloc_etherdev_mqs");
-    list.push_back("alloc_pages_current");
-    list.push_back("__alloc_skb");
-    list.push_back("dma_pool_alloc");
-    list.push_back("__kmalloc");
-    list.push_back("kmem_cache_alloc_trace");
-    list.push_back("__napi_alloc_skb");
-    list.push_back("netdev_alloc_frag");
-    list.push_back("__netdev_alloc_skb");
-    list.push_back("scsi_host_alloc");
-    list.push_back("vzalloc");
-
-    list.push_back("pci_get_device");
-    list.push_back("pci_get_domain_bus_and_slot");
-    list.push_back("pci_iomap");
-    list.push_back("pci_ioremap_bar");
-    list.push_back("snd_pci_quirk_lookup");
-
-    list.push_back("dma_pool_create");
+    // Note: should be consistent with list of functions in
+    // "guest/kernel-modules/kprobe_kernel_api"
+    list.insert("__alloc_ei_netdev");
+    list.insert("__alloc_pages_nodemask");
+    list.insert("__alloc_skb");
+    list.insert("__kmalloc");
+    list.insert("__napi_alloc_skb");
+    list.insert("__netdev_alloc_skb");
+    list.insert("__pskb_pull_tail");
+    list.insert("__request_region");
+    list.insert("alloc_etherdev_mqs");
+    list.insert("alloc_pages_current");
+    list.insert("build_skb");
+    list.insert("dev_get_drvdata");
+    list.insert("dma_pool_alloc");
+    list.insert("dma_pool_create");
+    list.insert("ioremap_nocache");
+    list.insert("kmem_cache_alloc_trace");
+    list.insert("netdev_alloc_frag");
+    list.insert("pci_get_device");
+    list.insert("pci_get_domain_bus_and_slot");
+    list.insert("pci_iomap");
+    list.insert("pci_ioremap_bar");
+    list.insert("scsi_host_alloc");
+    list.insert("sg_next");
+    list.insert("snd_ctl_new1");
+    list.insert("snd_info_create_card_entry");
+    list.insert("snd_pci_quirk_lookup");
+    list.insert("trace_event_buffer_reserve");
+    list.insert("vzalloc");
 
     return list;
 };
 
-const static vector<string> ptr_ret_funcs = init_ptr_ret_funcs();
+const static set<string> ptr_ret_funcs = init_ptr_ret_funcs();
 
 static inline
 bool match_ptr_ret_funcs(const string name)
 {
-    for(vector<string>::const_iterator it = ptr_ret_funcs.begin();
-            it != ptr_ret_funcs.end(); ++it) {
-        if(name.find(*it) != string::npos) return true;
+    // Note: xxx assumption on naming convention of concolic variable,
+    // e.g. __kmalloc[e100.module_core+0x3fff]
+    size_t ops = name.find('[');
+    assert(ops != string::npos);
+    if(ptr_ret_funcs.find(name.substr(0, ops)) != ptr_ret_funcs.end())
+    {
+        return true;
+    } else {
+        return false;
     }
-
-    return false;
 }
 
 // ------------------------------------------------
